26297cad8e30ae38911c5e13fbc77f83c31ccb05
MarcusLucky <marcus.v.m.santos2013@gmail.com>
2020-11-29 16:50:56 -0300

    melhoramento na pagina PRegister

--- site/PRegister.php
+++ site/PRegister.php
@@ -9,7 +9,8 @@
 
     <script src="https://kit.fontawesome.com/f6d182f726.js" crossorigin="anonymous"></script>
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="../styles/artista.css">
+    
+    <link rel="stylesheet" href="../styles/pessoal.css">
     <!--<link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,700;1,400&display=swap" rel="stylesheet">-->
 </head>
 <script>
@@ -41,7 +42,7 @@
 
             <div class="field">
 
-                <form method="POST" action="processa.php">
+                <form method="POST" action="processa.php" class="Layout-register">
 
 
 

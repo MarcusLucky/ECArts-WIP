f3df4ad2aff0bf98df896db3278921baf0b2651a
lmiguelm <luismiguelfernandes.marcelo@gmail.com>
2020-11-29 14:09:46 -0300

    editando informações pessoais

--- site/Config.php
+++ site/Config.php
@@ -49,48 +49,33 @@
 		<p><button class="up add-card" onclick="">Adiconar cartão</button></p>
 		<p> <input class="up back" type="button" value="voltar" onclick="history.back()"></p>
 	</div>
-	<form id="re">
+	<form method="POST" action="processa.php">
 		<table class="table-dados" align="center" style="border:4px double;" width="600" height="300">
 			<tr>
 				<th align="left"> Dados Pessoais</th>
 			</tr>
 			<tr>
-				<td align="right"> Novo Nome: </td>
+				<td align="right"> Nome: </td>
 				<td> <input class="input-form" type="text" required="" name="nome" size="30" placeholder="Nome completo"
-						maxlength="40"> </td>
+						maxlength="40"  value="<?php echo $_SESSION["usuario"]->getNome(); ?>"> </td>
 			</tr>
 
 			<tr>
-				<td align="right"> Nova senha:</td>
-				<td><input class="input-form password" type="password" name="Senha" size="30" maxlength="8"> </td>
+				<td align="right"> senha:</td>
+				<td><input class="input-form password" type="password" name="password" size="30" maxlength="8" value="<?php echo $_SESSION["usuario"]->getSenha(); ?>" /> </td>
 			</tr>
 
 			<tr>
 				<td align="right"> Tel:</td>
-				<td><input class="input-form" type="text" size="30" maxlength="9"> </td>
+				<td><input class="input-form" type="text" size="30" name="telefone" maxlength="9" value="<?php echo $_SESSION["usuario"]->getTelefone(); ?>"/> </td>
 			</tr>
 
 			<tr>
 				<td align="right"> E-Mail:</td>
-				<td><input class="input-form" type="text" name="email" size="30"></td>
+				<td><input class="input-form" type="text" name="email" value="<?php echo $_SESSION["usuario"]->getEmail(); ?>" size="30"/></td>
 			</tr>
 		</table>
 
-		<table class="table-conta" align="center" style="border:4px double;" width="600" height="50">
-			<tr>
-				<th valign="top" align="left"> tipo de conta:</th>
-			</tr>
-			<tr>
-				<td valign="top"> Conta: <br />
-					<select class="select-accont" name="serie">
-						<option> escolha o seu no registro</option>
-						<optgroup label="escolha">
-							<option> empresarial</option>
-							<option> Artistica</option>
-							<option> Usuario</option>
-					</select>
-			</tr>
-		</table>
 		<p align="center">
 			<br />
 			<label>
--- site/processa.php
+++ site/processa.php
@@ -1,26 +1,35 @@
 <?php  
-  
+  include_once("../classes/usuario.php");
   include_once("conexao.php");
 
   $nome = $_POST["nome"];
   $senha = $_POST["password"];
   $email = $_POST["email"];
   $telefone = $_POST["telefone"];
-  $profissao = $_POST["profession"];
-
-  $sql = "INSERT INTO usuarios(nome, email, senha, telefone, profissao ) VALUES ('$nome', '$email', '$senha', '$telefone', '$profissao')";
-
-  if (mysqli_query($conexao, $sql)) {
-    echo "New record created successfully";
-	header("location: login.php");
-  } else {
-    echo "Error: " . $sql . "<br>" . mysqli_error($conexao);
-  header("location: ARegister.php");
-  }
-
-  mysqli_close($conexao);
-
   
+  if(isset($_SESSION["usuario"])) {
+    $id = $_SESSION["usuario"]->getIdUsuario();
 
+    $sql = "UPDATE usuarios SET nome = '$nome', senha = '$senha', telefone = '$telefone', email = '$email' WHERE id_usuario = $id";
+    
+    if(mysqli_query($conexao, $sql)) {
+      $_SESSION["usuario"]->setNome($nome);
+      $_SESSION["usuario"]->setSenha($senha);
+      $_SESSION["usuario"]->setTelefone($telefone);
+      $_SESSION["usuario"]->setEmail($email);
+    } 
+    header("location: config.php");
+    
+  } else {
+    $profissao = $_POST["profession"];
 
+    $sql = "INSERT INTO usuarios(nome, email, senha, telefone, profissao ) VALUES ('$nome', '$email', '$senha', '$telefone', '$profissao')";
+
+    if (mysqli_query($conexao, $sql)) {
+      header("location: login.php");
+    } else {
+      header("location: ARegister.php");
+    }
+  }
+  mysqli_close($conexao);
 ?>

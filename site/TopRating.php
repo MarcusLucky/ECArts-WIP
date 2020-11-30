402991e1794497dd8a130cd125b1a8bc88d56ba3
MarcusLucky <marcus.v.m.santos2013@gmail.com>
2020-11-30 11:46:41 -0300

    arrumando as paginas principais

--- site/TopRating.php
+++ site/TopRating.php
@@ -14,7 +14,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ECArts - Principal</title>
+    <title>ECArts - Melhores Avaliados</title>
 
     <script src="https://kit.fontawesome.com/f6d182f726.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="../styles/styles.css">
